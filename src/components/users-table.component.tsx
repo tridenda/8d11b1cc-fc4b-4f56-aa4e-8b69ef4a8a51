c8acaa10bfe288f6e26a22f41c9b6878d0183645
Tri Denda <tridenda.nk@gmail.com>
2024-05-04 11:21:53 +0700

    finishing code

--- src/components/users-table.component.tsx
+++ src/components/users-table.component.tsx
@@ -314,8 +314,8 @@ const UsersTable = () => {
                           <path
                             fill="none"
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="m10.5 12.5l4 4.107l4-4.107m-8-4l-4-4l-4 3.997m4-3.997v12m8-12v12"
                           />
                         </svg>
